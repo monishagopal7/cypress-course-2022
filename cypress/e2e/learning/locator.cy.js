95eb23b2a6693a61034bc2429eb0ed3056bdbfb9
monishagopal7 <93284090+monishagopal7@users.noreply.github.com>
2022-11-28 18:56:47 -0800

    Update locator.cy.js

--- cypress/e2e/learning/locator.cy.js
+++ cypress/e2e/learning/locator.cy.js
@@ -1,17 +1,33 @@
 /// <reference types="cypress" />
 
 describe("learn about locators", ()=> {
-    it("visit googel home page",()=>{
+    it("visit google home page",()=>{
         //
 
     });
 
-    it("visit googel home page",()=>{ 
+    it("find the search button",()=>{ 
 
         cy.visit("https://google.com")
         //cy.get("input") //search by tag
         //cy.get('.gLFyf') //search by class
         //cy.get('input[title=Search]') //search by attibute
         cy.get('[name="q"]') //search by name
+        cy.get('.iblpc')
     })
+
+
+    it("assert-making sure the element is available",()=>{ 
+        cy.get('[name="q"]').should("be.visible") //making sure of the visibility
+    })
+
+    it("assert-making sure the class is there",()=>{ 
+        cy.get('.gLFyf',{timeout:5000})
+        .should("be.visible")
+        .and("have.class","gLFyf")
+        .and("have.value","")
+        .should("have.attr","maxlength","2048") //making sure of the visibility
+    })
+
+    //very important thing in timeout is assertion
 })
